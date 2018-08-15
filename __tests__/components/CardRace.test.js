e2444fad710a33d31f9fa728939466ec1a8063db
mateus-f-torres <mateusfigtorres@gmail.com>
2018-08-15 11:20:12 -0300

    add unit test for CardRace

--- __tests__/components/CardRace.test.js
+++ __tests__/components/CardRace.test.js
@@ -1,50 +1,82 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import CardRace from '../../src/components/CardRace.jsx';
+import RACES from '../../src/data/races.js';
 
 describe('<CardRace />', () => {
-  const race = {
-    name: 'Human',
-    id: 'human',
-    age: [20, 80],
-    size: 'Medium',
-    height: '6ft (1,7m)',
-    weight: '180lb (80kg)',
-    eyes: 'All human variations',
-    skin: 'All human variations',
-    hair: 'All human variations',
-    _placeholder: 'Salazar Marivaldi',
-    speed: 30,
-    lang: ['Common', '..'],
-    special: [
-      [
-        'Bonus Skill',
-        `You gain proficiency in one additional skill 
-        from your class skill list.`,
-      ],
-      [
-        'Magical Affinity',
-        `Whenever you are 5ft from a magical item you can sense 
-        its magic through one of your senses. 
-        Once per short rest you can increase the range of this feature 
-        to 30ft for 1min, after that you lose this feature and 
-        must finish a short rest to regain its passive effect. 
-        Using this feature only tells you if something is magical or not, 
-        nothing more than that.`,
-      ],
-      [
-        'Grit',
-        `You can use your reaction to gain advantage on a 
-        saving throw against a spell. 
-        You must roll a 5 or 6 on a d6 to use this feature again.`,
-      ],
-    ],
-  };
+  let race;
+  // helper for user interaction mocking
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  }
+  let randomCategory = randomRange(0, RACES.length - 1);
 
+  for (let i = 0; i < RACES.length; i++) {
+    if (i != randomCategory) continue;
+    let options = RACES[i][1];
+    let randomRace = randomRange(0, options.length - 1);
+    race = options[randomRace];
+  }
 
-  it('displays race name', () => {
+  it('displays correct race name', () => {
     const wrapper = shallow(<CardRace {...race}/>);
 
-    expect(wrapper.find('h2').text()).toEqual('Human');
+    expect(wrapper.find('[data-test="race-name"]')
+      .text()).toEqual(race.name);
+  });
+
+  it('displays correct race height', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-height"]')
+      .text()).toEqual(`Height:${race.height}.`);
+  });
+  it('displays correct race weight', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-weight"]')
+      .text()).toEqual(`Weight:${race.weight}.`);
+  });
+
+  it('displays correct race adulthood', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-adulthood"]')
+      .text()).toEqual(`Adulthood:${race.age[0]} years.`);
+  });
+
+  it('displays correct race lifespan', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-lifespan"]')
+      .text()).toEqual(`Lifespan:${race.age[1]} years.`);
+  });
+
+  it('displays correct race size', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-size"]')
+      .text()).toEqual(`Size:${race.size}.`);
+  });
+
+  it('displays correct race speed', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-speed"]')
+      .text()).toEqual(`Speed:${race.speed}ft.`);
+  });
+
+  it('displays correct race langs', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+
+    expect(wrapper.find('[data-test="race-langs"]')
+      .text()).toEqual(`Speaks:${race.lang[0]} & ${race.lang[1]}.`);
+  });
+
+  it('displays correct race traits', () => {
+    const wrapper = shallow(<CardRace {...race}/>);
+    expect(wrapper.find('[data-test="race-traits"]')
+      // add 1 to account for the h3 tag inside the ul
+      .children().length).toEqual(race.special.length + 1);
   });
 });
