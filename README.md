93e7ba4fe5d7fa135f307203dc7a40e16bf588ff
mateus-f-torres <mateusfigtorres@gmail.com>
2018-08-20 16:31:23 -0300

    unit test score reducer

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+# Argilizando
+Homebrew character creator for DnD 5e 
+
+## Description
+
+Argilizando is a helper for my DnD games. It provides me, my players and anyone else that uses it with my idea of  a more streamlined approach to the character creation process. Allowing players to quickly make a rought draft of a character, it is impleaded that whoever uses it already owns or has acess to books needed to truly flseh out and play with the character.
+ If you a truly complete online tool please try out  https://www.dndbeyond.com/ ,the official digital toolset for Dungeons and Dragons Fifth Edition
+
+## Usage
+
+## Contributing
+
+## Credits
+
+## Disclaimer
+
+I do not own or claim to own any
+ 
+ Copyright Disclaimer Under Section 107 of the Copyright Act 1976
+ non-profit
+ educational
+ personal use app	
+ http://dnd.wizards.com/
 
--- __tests__/setup/helpers.js
+++ __tests__/setup/helpers.js
@@ -1,4 +1,3 @@
-
 export const getAsset = (assetID, database) => {
   for (const category of database) {
     for (const item of category[1]) {
@@ -7,3 +6,33 @@ export const getAsset = (assetID, database) => {
   }
 };
 
+export const actionCreator = (actionType) => {
+  return function action(actionPayload) {
+    return actionPayload
+      ? {
+        type: actionType,
+        payload: actionPayload,
+      }
+      : {
+        type: actionType,
+      };
+  };
+};
+
+export const actionRepeater = (action, times, reducer, starter) => {
+  let state = starter
+    ? starter
+    : reducer(undefined, {type: 'dummy'});
+  for (let i = 0; i < times; i++) {
+    state = reducer(state, action);
+  }
+  return state;
+};
+
+export const actionScriptRunner = (script, reducer) => {
+  let state = reducer(undefined, {type: 'dummy'});
+  for (const action of script) {
+    state = reducer(state, action);
+  }
+  return state;
+};
